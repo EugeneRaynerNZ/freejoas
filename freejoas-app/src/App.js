1c297a656d0664d41b47bd74aab89076f98707b2
Shuai Wang <wsking233@gmail.com>
2024-02-28 00:39:50 +1300

    add google login component

--- freejoas-app/src/App.js
+++ freejoas-app/src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import Navigation from "./Navigation";
 import Body from "./components/Body";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import GoogleOAuth from './client_secret.json';
+
 
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
-      <Body />
+      <GoogleOAuthProvider clientId={GoogleOAuth.web.client_id}>
+        <Navigation />
+        <Body />
+      </GoogleOAuthProvider>
     </BrowserRouter>
   );
 }
--- freejoas-app/src/pages/Home.js
+++ freejoas-app/src/pages/Home.js
@@ -1,19 +1,35 @@
+import React from 'react';
 import image from '../freejoas-image.png';
 import '../App.css';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
+
+
 
 function Home() {
   return (
 
-      <main>
-        <div>
-          <h2>Find your</h2>
-          <h1 className="text-3xl font-bold underline">Freejoas</h1>
-          <h2>Today</h2>
-        </div>
-        <div>
-          <img src={image} alt="Freejoas"/>
-        </div>
-      </main>
+    <main>
+      <div>
+        <h2>Find your</h2>
+        <h1 className="text-3xl font-bold underline">Freejoas</h1>
+        <h2>Today</h2>
+      </div>
+      <div>
+        <img src={image} alt="Freejoas" />
+      </div>
+
+      <GoogleLogin
+        onSuccess={credentialResponse => {
+          console.log(credentialResponse);
+          const decoded = jwtDecode(credentialResponse.credential);
+          console.log(decoded);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+    </main>
 
   );
 }
