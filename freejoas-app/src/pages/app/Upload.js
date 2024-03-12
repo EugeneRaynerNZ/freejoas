affa47b1436c2887d24462ed9ec0ecea0b1f65e4
Eugene Rayner <rayner.euge@gmail.com>
2024-03-12 22:47:02 +1300

    updated upload screen as per designs

--- freejoas-app/src/pages/app/Upload.js
+++ freejoas-app/src/pages/app/Upload.js
@@ -53,30 +53,48 @@ function Upload() {
     }
       
     return (
-       <div className="flex flex-col w-full pt-8" id="upload">
+       <div className="flex flex-col w-full pt-8 main-container">
 
-       <div className="flex-1 flex flex-col gap-2 mx-8">
-        <div className="flex gap-16 flex-col max-w-xl my-0">
-          <h4 className="text-center">Add a new location</h4>
-            <div className="w-full">
-              <h4 className="text-center mb-4">Enter from my current location</h4>
-              <button className="bg-green-700 text-white rounded p-2 w-full cta--button" onClick={getCurrentLocation}>Get my current location</button>
-            </div>
-            <div className="w-full">
-              <h4 className="text-center mb-4">Enter in precise coorindates</h4>
-              <form className="flex flex-col gap-2 mb-16">
-                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="latitude" value={inputs.latitude || ''} onChange={handleChange} placeholder="lat"/>
-                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="longitude" value={inputs.longitude || ''} onChange={handleChange} placeholder="lng"/>
-                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="amount" placeholder="amount of trees" value={inputs.amount || ''} onChange={handleChange} />
-                <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="title" placeholder="title" value={inputs.title || ''} onChange={handleChange} />
-              </form>
-              <button className="bg-green-700 text-white rounded p-2 w-full cta--button" onClick={handleClick}>Add a new location now</button>
-            </div>
+       <div className="flex-1 flex flex-col gap-2 mx-8 main-container--top">
+        <div className="flex gap-8 flex-col max-w-xl my-0">
+          <div className="flex flex-col gap-8 w-full">
+            <p className="page-title">Upload</p>
           </div>
+          <div className="w-full">
+            <form className="flex flex-col gap-6 mb-16">
+              <div className="flex flex-col gap-4">
+                <label className="input--container">
+                    <span>Your Latitude</span>
+                    <input type="text" name="latitude" value={inputs.latitude || ''} onChange={handleChange} placeholder="-34.2512" />
+                </label>
+                <label className="input--container">
+                    <span>Your Longitude</span>
+                    <input type="text" name="longitude" value={inputs.longitude || ''} onChange={handleChange} placeholder="-32.5123" />
+                </label>
+                <div onClick={getCurrentLocation} className="get-my-location">Get my location</div>
+              </div>
+              <label className="input--container">
+                  <span>How many trees?</span>
+                  <input type="text" name="amount" value={inputs.amount || ''} onChange={handleChange} placeholder="2" />
+              </label>
+              <label className="input--container">
+                  <span>Name the location</span>
+                  <input type="text" name="title" value={inputs.title || ''} onChange={handleChange} placeholder="Snazzy Location Name" />
+              </label>
+              {/* <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="latitude" value={inputs.latitude || ''} onChange={handleChange} placeholder="lat"/>
+              <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="longitude" value={inputs.longitude || ''} onChange={handleChange} placeholder="lng"/>
+              <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="amount" placeholder="amount of trees" value={inputs.amount || ''} onChange={handleChange} />
+              <input className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="title" placeholder="title" value={inputs.title || ''} onChange={handleChange} /> */}
+            </form>
+            <button className="bg-green-700 text-white rounded p-2 w-full cta--button" onClick={handleClick}>Upload Location</button>
+          </div>
+        </div>
        </div>
 
         
-        <Navigation />
+        <div className="main-container--bottom">
+          <Navigation />
+        </div>
         </div> 
     );
 }
