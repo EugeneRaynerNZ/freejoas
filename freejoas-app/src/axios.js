dbfa2cdc8202a82c69de866a5faab152cb92c513
Shuai Wang <wsking233@gmail.com>
2024-03-14 02:59:52 +1300

    delete backend, and add token to the header

--- freejoas-app/src/axios.js
+++ freejoas-app/src/axios.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWU2OWYzNWQwNzAxODczZGU5NjQyNTMiLCJhY2NvdW50VHlwZSI6ImFkbWluIiwiaWF0IjoxNzEwMzI5MTAwNDIwLCJleHAiOjE3MTA0MTU1MDA0MjB9.D9IPHRh7Zalmqfq34-SXjzBifkXHgUzphYNyf0t5t80";
+
 // this is the backend ngrok URL (this will change)
 const instance = axios.create({
-    // baseURL: "http://localhost:4000/api",
-    baseURL: "https://freejoas.azurewebsites.net/api/v1",
+    baseURL: "http://localhost:4000/api/v1",
+    // baseURL: "https://freejoas.azurewebsites.net/api/v1",
     headers: {
         'Content-Type': 'application/json',
+        "Authorization": "Bearer " + token,
         // 'ngrok-skip-browser-warning' :'skip-browser-warning'
     }
 })
