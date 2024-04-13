5d13de7dc40e1bcb096201e66fef6423a1602a4f
Shuai Wang <wsking233@gmail.com>
2024-04-13 19:21:37 +1200

    remove un used import

--- freejoas-app/src/pages/start/Login.js
+++ freejoas-app/src/pages/start/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../../App.css';
 import axios from '../../axios';
 import { NavLink } from "react-router-dom";
