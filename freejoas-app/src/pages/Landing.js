e888e44890f48000d4add455d34ddd762cd383f5
Shuai Wang <wsking233@gmail.com>
2024-07-01 00:39:29 +1200

    Update Landing.js

--- freejoas-app/src/pages/Landing.js
+++ freejoas-app/src/pages/Landing.js
@@ -12,7 +12,7 @@ function Landing() {
             <div className="landing-page--text-container">
                 <h1>Let's find some free Feijoas!</h1>
                 <p>If you live in New Zealand, sign up to find freejoas near you.</p>
-                <NavLink className="cta--button cta--button-primary" to="/home" >Explor Now</NavLink>
+                <NavLink className="cta--button cta--button-primary" to="/home" >Explore Now</NavLink>
             </div>
             <div className="landing-page--image-container">
                 <img src={LandingPageImage} alt="Feijoa Tree"/>
