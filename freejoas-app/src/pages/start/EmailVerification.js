0fa6745943054267c28fd24296eb204326fd5f74
Eugene Rayner <rayner.euge@gmail.com>
2024-07-03 20:17:40 +1000

    added resent text

--- freejoas-app/src/pages/start/EmailVerification.js
+++ freejoas-app/src/pages/start/EmailVerification.js
@@ -10,6 +10,7 @@ function EmailVerification() {
         <h1 className="text-center mb-4">Check your email inbox</h1>
         <img className="text-center" src={PaperPlane} alt="Paper Plane" />
         <p className="text-center">Check your inbox for instructions from us on how to verify your account.</p>
+        <p className="text-center">Didn't receive the email? <a href="#resend">Resend</a></p>
       </section>
 
   );
