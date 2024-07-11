b9658aef12e4447efc69ee652065dace5cb66969
Shuai Wang <wsking233@gmail.com>
2024-07-11 18:14:25 +1200

    change the google map dependency

--- freejoas-app/src/components/GoogleMap.js
+++ freejoas-app/src/components/GoogleMap.js
@@ -1,64 +1,95 @@
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import mapStyles from './GoogleMapStyles';
-import config from '../utils/config';
-
+import React, { useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+import mapStyles from "./GoogleMapStyles";
+import config from "../utils/config";
 
 const containerStyle = {
-    width: '100%',
-    height: '400px'
+  width: "100%",
+  height: "400px",
 };
 
 const center = {
-    lat: -36.8571789,
-    lng: 174.7389711
+  lat: -36.8571789,
+  lng: 174.7389711,
 };
 
 const myPosition = {
-    lat: -36.8571789,
-    lng: 174.7389711
+  lat: -36.8571789,
+  lng: 174.7389711,
+};
+
+const mapOptions = {
+  styles: mapStyles,
+  streetViewControl: false,
+  fullscreenControl: false,
+  mapTypeControl: false,
+  mapTypeId: "terrain",
 };
 
 const MapContainer = ({ data }) => {
-    return (
-        <LoadScript
-            googleMapsApiKey={config.REACT_APP_GOOGLE_MAPS_API_KEY}
+  const [infoWindowOpen, setInfoWindowOpen] = useState(false);
+
+  return (
+    <APIProvider apiKey={config.REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <div style={containerStyle}>
+        <Map
+          zoom={12}
+          center={center}
+          mapId={config.REACT_APP_GOOGLE_MAPS_ID}
+          mapOptions={mapOptions}
         >
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={12}
-                options={{
-                    styles: mapStyles,
-                    streetViewControl: false,
-                    fullscreenControl: false,
-                    mapTypeControl: false,
-                    mapTypeId: 'terrain',
-                }}
+          <AdvancedMarker
+            position={myPosition}
+            onClick={() => setInfoWindowOpen(true)}
+          ></AdvancedMarker>
+          {infoWindowOpen && (
+            <InfoWindow
+              position={myPosition}
+              onCloseClick={() => setInfoWindowOpen(false)}
             >
-               
-                { /* Child components, such as markers, info windows, etc. */}
-                {data.map((point, index) => {
-                    const lat = parseFloat(point.latitude);
-                    const lng = parseFloat(point.longitude);
-                    if (!isNaN(lat) && !isNaN(lng)) {
-                        console.log('Valid latitude and longitude');
-                        console.log(lat, lng);
-                        return (
-                            <Marker
-                                key={index}
-                                position={{ lat: lat, lng: lng }}
-                            />
-                        );
-                    }
-                    console.log('Invalid latitude or longitude');
-                    return null;
-                })}
-            </GoogleMap>
-        </LoadScript>
-    );
-}
+              <div>
+                <h1>My Location </h1>
+                <h2>Lat: {myPosition.lat}</h2>
+                <h2>Lng: {myPosition.lng}</h2>
+              </div>
+            </InfoWindow>
+          )}
+        </Map>
+      </div>
+    </APIProvider>
 
-
+    // <LoadScript
+    //     googleMapsApiKey={config.REACT_APP_GOOGLE_MAPS_API_KEY}
+    //     loadingElement={<div>Loading...</div>}
+    //      version="weekly"
+    // >
+    //   <GoogleMap
+    //     mapContainerStyle={containerStyle}
+    //     center={center}
+    //     zoom={12}
+    //     options={mapOptions}
+    //   >
+    //     {/* Child components, such as markers, info windows, etc. */}
+    //     {/* <Marker
+    //                 position={myPosition}
+    //            ></Marker> */}
+    //     {data.map((point, index) => {
+    //       const lat = parseFloat(point.latitude);
+    //       const lng = parseFloat(point.longitude);
+    //       if (!isNaN(lat) && !isNaN(lng)) {
+    //         return <Marker key={index} position={{ lat: lat, lng: lng }} />;
+    //       }
+    //       console.log("Invalid latitude or longitude");
+    //       return null;
+    //     })}
+    //   </GoogleMap>
+    // </LoadScript>
+  );
+};
 
 export default MapContainer;
--- freejoas-app/src/utils/config.js
+++ freejoas-app/src/utils/config.js
@@ -1,6 +1,9 @@
 const config = {
+    // google map related
     REACT_APP_GOOGLE_MAPS_API_KEY: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    REACT_APP_GOOGLE_MAPS_ID: process.env.REACT_APP_GOOGLE_MAPS_ID,
 
+    // backend related
     REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL,
 }
 
