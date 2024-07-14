41f6c346e79f9c31b083994030f26414a11e2689
Eugene Rayner <rayner.euge@gmail.com>
2024-07-14 11:49:45 +1000

    updated dashboard screen for desktop only

--- freejoas-app/src/Navigation.js
+++ freejoas-app/src/Navigation.js
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { ReactComponent as HomeIcon } from './images/navigation/home.svg';
 import { ReactComponent as ExploreIcon } from './images/navigation/explore.svg';
 import { ReactComponent as UploadIcon } from './images/navigation/upload.svg';
+import SmallLogo from './images/desktop/app/small-logo.svg';
 
 // import HomeIcon from '@mui/icons-material/Home';
 // import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
@@ -14,31 +15,34 @@ import { ReactComponent as UploadIcon } from './images/navigation/upload.svg';
 
 function Navigation() {
   return (
-    <footer className="flex">
+    <div className="navigation--container flex">
+      <div className="">
+        <img src={SmallLogo} alt="Freejoas Logo"/>
+      </div>
       <nav className="flex text-center">
         <li className="nav-link nav-link--home">
           <NavLink to="/dashboard" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
             {({ isActive }) => (
-              <>{isActive ? <HomeIcon fontSize="large" /> : <HomeIcon fontSize="large" />} Home</>
+              <>{isActive ? <HomeIcon fontSize="large" /> : <HomeIcon fontSize="large" />} <span>Home</span></>
             )}
           </NavLink>
         </li>
         <li className="nav-link nav-link--play">
           <NavLink to="/play" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
             {({ isActive }) => (
-              <>{isActive ? <ExploreIcon fontSize="large" /> : <ExploreIcon fontSize="large" />} Explore</>
+              <>{isActive ? <ExploreIcon fontSize="large" /> : <ExploreIcon fontSize="large" />} <span>Explore</span></>
             )}
           </NavLink>
         </li>
         <li className="nav-link nav-link--upload">
           <NavLink to="/upload" className={({ isActive, isPending }) => (isPending ? "pending" : isActive ? "active" : "")}>
             {({ isActive }) => (
-              <>{isActive ? <UploadIcon fontSize="large" /> : <UploadIcon fontSize="large" />} Upload</>
+              <>{isActive ? <UploadIcon fontSize="large" /> : <UploadIcon fontSize="large" />} <span>Upload</span></>
             )}
           </NavLink>
         </li>
       </nav>
-    </footer>
+    </div>
   );
 }
 
